8cbc8153c70c117e77451720758562a43b2734b5
Clemens Akens <clebert@me.com>
2019-10-09 23:11:14 +0200

    Update dependency aws-simple to ^0.0.9

--- aws-simple.config.js
+++ aws-simple.config.js
@@ -5,12 +5,14 @@
  */
 exports.default = {
   appName: 'aws-simple-example',
-  stackName: 'prod',
+  defaultStackName: 'prod',
+  region: 'eu-central-1',
   customDomainConfig: {
     certificateArn: process.env.CERTIFICATE_ARN,
     hostedZoneId: process.env.HOSTED_ZONE_ID,
     hostedZoneName: 'clebert.io',
-    aliasRecordName: 'aws-simple-example'
+    getAliasRecordName: stackName =>
+      `aws-simple-example${stackName === 'prod' ? '' : `-${stackName}`}`
   },
   minimumCompressionSize: 1000,
   loggingLevel: 'INFO',
